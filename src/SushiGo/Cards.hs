4f32fc6270daec74478927a7d8c0e08b38a00a23
Jordaniel Wolk <jd@philosophie.is>
2016-07-31 05:21:56 -0700

    WIP compiles; bug w/ maki nums

--- src/SushiGo/Cards.hs
+++ src/SushiGo/Cards.hs
@@ -7,13 +7,14 @@ module SushiGo.Cards (
 , score
 , group
 , groupSashimi
+, getCards
 , separateCardTypes
 ) where
 
 import           Data.List       (concatMap, dropWhile, groupBy, head, sort,
                                   takeWhile)
 import qualified Data.Map.Strict as M
-import           Data.Maybe      (fromJust)
+import           Data.Maybe      (fromMaybe)
 import           Data.Monoid     ((<>))
 
 data Card = Sashimi
@@ -85,7 +86,7 @@ group cs = sashimiGroups
         makiGroups = groupMaki $ getCards (Maki 1) separated
 
 getCards :: Card -> M.Map Card [Card] -> [Card]
-getCards c sortedCs = fromJust $ M.lookup c sortedCs
+getCards c sortedCs = fromMaybe [] $ M.lookup c sortedCs
 
 separateCardTypes :: [Card] -> M.Map Card [Card]
 separateCardTypes cs = M.fromList zipped
@@ -107,11 +108,16 @@ groupTempura cs = tHelp cs mempty
 
 -- Dumpling groups have >= 1 dumplings
 groupDumplings :: CardsToPlay -> GroupResult Card
-groupDumplings cs = GroupResult [(DumplingGroup (length dumplings))] notDumplings
-  where dumplings = (takeWhile (== Dumpling) cs)
+groupDumplings cs = GroupResult dumplingGroups notDumplings
+  where dumplingGroups = if numDumplings > 0 then [DumplingGroup numDumplings] else []
+        numDumplings = length dumplings
+        dumplings = (takeWhile (== Dumpling) cs)
         notDumplings = (dropWhile (== Dumpling) cs)
 
 groupMaki :: CardsToPlay -> GroupResult Card
-groupMaki cs = GroupResult [(MakiGroup $ sum (map numMaki maki))] notMaki
-  where maki = takeWhile (== Maki 1) cs
-        notMaki = dropWhile (== Maki 1) cs
+groupMaki cs = GroupResult makiGroups notMaki
+  where makiGroups = if makiCount > 0 then [MakiGroup $ sum (map numMaki maki)] else []
+        makiCount = length maki
+        maki = (takeWhile (== (Maki 1)) cs)
+        notMaki = (dropWhile (== (Maki 1)) cs)
+
